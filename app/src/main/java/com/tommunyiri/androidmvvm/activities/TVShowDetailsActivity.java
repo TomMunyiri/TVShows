9f91b57073701da90d9b291329139951b616337f
tom <tmmunyiri@gmail.com>
2021-01-10 14:11:55 +0300

    slider indicators

--- app/src/main/java/com/tommunyiri/androidmvvm/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/tommunyiri/androidmvvm/activities/TVShowDetailsActivity.java
@@ -1,11 +1,15 @@
 package com.tommunyiri.androidmvvm.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.tommunyiri.androidmvvm.R;
@@ -47,5 +51,37 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         activityTVShowDetailsBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
         activityTVShowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityTVShowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
+    }
+
+    private void setupSliderIndicators(int count){
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8,0,8,0);
+        for(int i=0;i<indicators.length;i++){
+            indicators[i]=new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),R.drawable.background_slider_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTVShowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTVShowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+    }
+
+    private void setCurrentSliderIndicator(int position){
+        int childCount = activityTVShowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for(int i=0;i<childCount;i++){
+            ImageView imageView=(ImageView)activityTVShowDetailsBinding.layoutSliderIndicators.getChildAt(i);
+            if(i==position){
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.background_slider_indicator_active));
+            }else{
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                        getApplicationContext(),R.drawable.background_slider_indicator_inactive
+                ));
+            }
+        }
     }
 }
